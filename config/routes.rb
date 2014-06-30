6a13bbd7d911bef0bedfc2ff6edef7513b77c5c8
Andrew Slawter <aslawter@gmail.com>
2014-06-30 12:09:27 -0400

    Create dashboard view with all shouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+require "monban/constraints/signed_in"
+
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
+  resources :shouts, only: [:create]
+
+  constraints Monban::Constraints::SignedIn.new do
+    get "/", to: "dashboards#show", as: :dashboard
+  end
 
   root to: "sessions#new"
   get "/sign_up", to: "users#new"
---
+++ db/migrate/20140630144005_create_shout.rb
@@ -0,0 +1,9 @@
+class CreateShout < ActiveRecord::Migration
+  def change
+    create_table :shouts do |t|
+      t.text :body, null: false
+      t.belongs_to :user, index: true, null: false
+      t.timestamps null: false
+    end
+  end
+end
