ba9bf7e98233ebe5e2703b9e9d4bd8f02b0b16b2
Andrew Slawter <aslawter@gmail.com>
2014-07-02 16:08:01 -0400

    WIP - Modify all session and new user controllers and views
    *Change email to username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = authenticate_session(session_params, floomp: [:username])
+    user = authenticate_session(session_params, username: [:username])
 
     if sign_in(user)
       redirect_to root_path
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:username, :password)
   end
 end
 
