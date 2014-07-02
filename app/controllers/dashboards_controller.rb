43010c279f9464568704fce7580f0d59cbbd7c85
Andrew Slawter <aslawter@gmail.com>
2014-07-02 13:09:01 -0400

    Display followed user shouts on dashboard
    *Display followed user shouts
    *Order chronologically, decending
    *Ran db migration to create unique constraint on user email
    *Modify dashboard layout for cleaner appearance and display time ago in
    words

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @text_subject = TextSubject.new 
+    followed_users = current_user.followed_users
+    @shouts = Shout.where(user_id: followed_users).order(created_at: :desc)
   end
 end
