ad41931d9acb78a26a45a4317ce04a6f238ce80f
Andrew Slawter <aslawter@gmail.com>
2014-07-07 08:08:06 -0400

    Create index page of all users.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require "monban/constraints/signed_in"
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
-  resources :users, only: [:new, :create] do
+  resources :users, only: [:index, :new, :create] do
     resource :following_relationship, only: [:create, :destroy]
   end
 
